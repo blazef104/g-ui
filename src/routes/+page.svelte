4262fce8bbbf72e132d5b9f8c05761d730b4a637
blazef104 <nanc23@rocketmail.com>
2024-04-21 22:57:42 +0100

    Setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+	import Button from '../lib/Buttons.svelte';
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- TEST -->
+<div style="margin-left: 0.2rem;">
+	<Button>Test</Button>
+</div>
