c2710873fbc5e5f9534a62f9c53c33b6d28cf3fb
blazef104 <nanc23@rocketmail.com>
2024-05-17 10:04:33 +0100

    First attempt at packaging

---
+++ src/lib/Toaster.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Toast from './Toast.svelte';
+	import type { ToastStack } from './types.ts';
+
+	export let toasts: ToastStack = [
+		{ severity: 'error', message: 'Error' },
+		{ severity: 'info', message: 'Info' },
+		{ severity: 'success', message: 'Success' },
+		{ severity: 'warning', message: 'Warning' }
+	]; // Convert to use a store
+</script>
+
+<div class="absolute right-2 bottom-2 space-y-2">
+	{#each toasts as toast}
+		<Toast type={toast.severity} message={toast.message}></Toast>
+	{/each}
+</div>
